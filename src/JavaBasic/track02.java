6b5769e2c99b11ed96dc03575192678d0a386a22
RaminImani <Raminimani01@gmail.com>
2021-10-31 01:10:38 -0400

    The commit has edited to track02 file

--- src/JavaBasic/track02.java
+++ src/JavaBasic/track02.java
@@ -3,7 +3,20 @@ package JavaBasic;
 public class track02 {
 public static void main(String[] args) {
 	
-	System.out.println("Hello World");
+	double morgagerate=4.4;
+	int morgageprice=190000;
+	
+	if (morgagerate<4.5) {
+		System.out.println("User will consider buying");
+		if (morgageprice>200000) {
+			System.out.println("The User will take loan");
+		}else {
+			System.out.println("User will pay Cash");
+			}
+
+	}else {
+		System.out.println("User will not buy a house");
+	}
 }
 
 }
